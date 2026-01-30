cee8e643b4bd47173bf6c7f94b401a2c7388e3fd
khaled <khaledhmmddd123@gmail.com>
2026-01-30 03:37:42 +0200

    what sort of sorcery is this?

---
+++ parsing/validation.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   validation.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: khhammou <khhammou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/30 02:11:22 by khhammou          #+#    #+#             */
+/*   Updated: 2026/01/30 02:11:25 by khhammou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	is_valid_number(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	if (!str[i])
+		return (0);
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	has_duplicates(int *arr, int size)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < size)
+	{
+		j = i + 1;
+		while (j < size)
+		{
+			if (arr[i] == arr[j])
+				return (1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	has_spaces(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == ' ')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+void	free_split(char **split)
+{
+	int	i;
+
+	if (!split)
+		return ;
+	i = 0;
+	while (split[i])
+	{
+		free(split[i]);
+		i++;
+	}
+	free(split);
+}
+
+int	count_numbers(char **av, int start)
+{
+	int		total;
+	int		i;
+	char	**pieces;
+	int		j;
+
+	total = 0;
+	i = start;
+	while (av[i])
+	{
+		if (has_spaces(av[i]))
+		{
+			pieces = ft_split(av[i], ' ');
+			j = 0;
+			while (pieces[j++])
+				total++;
+			free_split(pieces);
+		}
+		else
+			total++;
+		i++;
+	}
+	return (total);
+}
