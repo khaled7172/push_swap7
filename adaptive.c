0878ff4ff6f52c01c19f17f0f3743e362c01de2c
Khaled Hammoud <khhammou@c1r6s5.42beirut.com>
2026-01-31 21:52:28 +0200

    kill

--- adaptive.c
+++ adaptive.c
@@ -6,7 +6,7 @@
 /*   By: khhammou <khhammou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/29 20:17:37 by kali              #+#    #+#             */
-/*   Updated: 2026/01/30 02:08:13 by khhammou         ###   ########.fr       */
+/*   Updated: 2026/01/31 21:49:37 by khhammou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 void	sort_adaptive(t_node **a, t_node **b, double disorder)
 {
-	if (disorder < 0.2)
+	if (disorder < 0.35)
 		sort_simple(a, b);
-	else if (disorder < 0.5)
+	else if (disorder < 0.6)
 		sort_medium(a, b);
 	else
 		sort_complex(a, b);
