e956c7d531579e78ef4900f3ab60294d92acd581
Khaled Hammoud <khhammou@c1r6s5.42beirut.com>
2026-01-31 23:24:33 +0200

    i wanna play tennis too

--- adaptive.c
+++ adaptive.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   adaptive.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kali <kali@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: khhammou <khhammou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/29 20:17:37 by kali              #+#    #+#             */
-/*   Updated: 2026/01/31 22:05:13 by kali             ###   ########.fr       */
+/*   Updated: 2026/01/31 23:20:56 by khhammou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,18 @@
 
 void	sort_adaptive(t_node **a, t_node **b, double disorder)
 {
-	if (disorder < 0.5)
+	int	size;
+
+	size = stack_size(*a);
+	if (size <= 3)
 		sort_simple(a, b);
-	else if (disorder < 0.9)
+	else if (size <= 5)
+		sort_simple(a, b);
+	else if (size <= 100 && disorder < 0.6)
+		sort_medium(a, b);
+	else if (size <= 100)
+		sort_complex(a, b);
+	else if (disorder < 0.7)
 		sort_medium(a, b);
 	else
 		sort_complex(a, b);
