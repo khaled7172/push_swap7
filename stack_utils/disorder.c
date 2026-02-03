e9064fe9d1b557e0dae8f64a98bb80e49aef129b
khaled <khaledhmmddd123@gmail.com>
2026-02-03 11:38:10 +0200

    modified code erroring on int max and int min values which are valid and optimized order calculation to get list size one time instead of iterating it twice, added check when building the stack that checks if the array passed was empty or if the nb of integers was less than or equal to zero to return NULL

--- stack_utils/disorder.c
+++ stack_utils/disorder.c
@@ -6,7 +6,7 @@
 /*   By: kali <kali@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/28 21:20:04 by kali              #+#    #+#             */
-/*   Updated: 2026/02/01 21:01:17 by kali             ###   ########.fr       */
+/*   Updated: 2026/02/03 10:45:42 by kali             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,13 @@ double	calculate_disorder(t_node *stack)
 	t_node	*runner;
 	int		inversions;
 	int		total_pairs;
+	int		size;
 
 	inversions = 0;
-	total_pairs = stack_size(stack) * (stack_size(stack) - 1) / 2;
+	size = stack_size(stack);
+	total_pairs = size * (size - 1) / 2;
+	if (total_pairs == 0)
+		return (0.0);
 	current = stack;
 	while (current)
 	{
@@ -33,7 +37,5 @@ double	calculate_disorder(t_node *stack)
 		}
 		current = current->next;
 	}
-	if (total_pairs == 0)
-		return (0.0);
 	return ((double)inversions / total_pairs);
 }
